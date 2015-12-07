a2fdc2c14754d5db4672447ebf5d1ff0cd66a771
ingcesarvelasquez <cesarandresvelasquez01@gmail.com>
2015-12-07 01:02:50 -0500

    Commit 4-3 Conexion entre la coleccion Posts y la plantilla postList

--- client/templates/posts/posts_List.js
+++ client/templates/posts/posts_List.js
@@ -1,25 +1,6 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		url:  'http://sachagreif.com/introducing-telescope/'
-	},
-
-	{
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	},
-
-	{
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
-	},
-	{
-		title: 'Descubriendo Meteor',
-		url: 'http://es.discovermeteor.com/'
-	}
-
-];
 Template.postsList.helpers({
-	posts: postsData
+	posts: function () {
+		return Posts.find();
+	}
 });
 
